d4fbfc9d097c1bd5ce27a1cfca84cb7ff357bd22
Irfan Ullah <154457581+IrfanUllah312@users.noreply.github.com>
2025-02-20 21:49:23 +0500

    Add files via upload

--- mycode.asm
+++ mycode.asm
@@ -1,20 +1,56 @@
 
-
-org 100h
-
+org 100h  
 
 
 
-MOV AX, 0001h
-MOV BX, 0001h
+;=============QUESTION 1=============
 
-MUL BX         
+;mov AX,19 
+;mul AX
+;add AX,3 
+;sub AX,29 not possible 
 
-MOV DL, AL    
+;=============QUESTION 2=============
+;;mov Al,3 
+;mov AL
 
-            
+;not possible because we will  GET 9*9 
 
 
+;===========QUESTION 3==========
+;mov AL,3
+;mul AL 
+;mul AL
+;mov BL,2
+;mov Cl,3
+;mov Cl 
+;add BL,4
+;ADD BL,9
+;ADD AL,BL 
+
+
+
+;===========QUESTION 4==========
+;mov AL,5
+;mov BL,3
+;ADD  AL,BL
+;mov AH,AL
+ 
+;===========QUESTION 5========== 
+
+;mov AX,1
+;mov BX,5
+;MUL BX 
+
+
+;===========QUESTION 6==========
+
+
+;===========QUESTION 6==========
+
+;mov AX,10
+;mov BX,5
+;DIV BX
 
 ret
 
